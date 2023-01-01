7ef87cc5ddb2223198cd8c1a15ac0ce63bf3a51a
forthexp <forthexp@mail.com>
2023-01-02 00:58:20 +0530

    Script to add vimrc

---
+++ setup_vim.sh
@@ -0,0 +1,11 @@
+cat << EOF > ~/.vimrc
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.scrolloff = 8
+EOF
