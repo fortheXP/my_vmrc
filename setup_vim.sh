1d47559cbb76a6f5b58ea62672a00685c63ed265
forthexp <forthexp@mail.com>
2023-01-02 09:27:27 +0530

    added a sepaarte script

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,11 +1,1 @@
-cat << EOF > ~/.vimrc
-set nu = true
-set relativenumber = true
-
-set tabstop = 4
-set softtabstop = 4
-set shiftwidth = 4
-set expandtab = true
-
-set scrolloff = 8
-EOF
+cp vimrc ~/.vimrc
---
+++ vimrc
@@ -0,0 +1,9 @@
+set nu = true
+set relativenumber = true
+
+set tabstop = 4
+set softtabstop = 4
+set shiftwidth = 4
+set expandtab = true
+
+set scrolloff = 8
